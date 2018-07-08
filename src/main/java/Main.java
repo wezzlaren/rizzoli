0e236b1f7012e503a4f6d75c2715d27cf355f7c4
Rizzoli <lrizzoli74@gmail.com>
2018-07-08 17:13:45 +0100

    idc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     }
 
 
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
